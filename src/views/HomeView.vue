6860369d77326f0396cafced8bd0b3fb05a53021
116-lattawit <89565120+Lattawit071@users.noreply.github.com>
2024-05-16 12:41:04 +0700

    ui filter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -137,33 +137,39 @@ function alertMessageHandler(title, status, data, type = "success") {
           <h1>{{ toggleManage }}</h1>
         </div>
         <div
-          class="flex justify-between w-[202px] h-[45px] px-[10px] m-[auto] bg bg-[#F9F9F9] border border-[#BDBDBD] rounded-[4px]"
+          class="bg-gray-900 flex justify-between w-[202px] h-[45px] px-[10px] m-[auto] bg border border-[#BDBDBD] rounded-[4px]"
         >
           <div class="dropdown dropdown-bottom text-xs font-medium">
             <div
               tabindex="0"
               role=""
-              class="cursor-pointer w-full h-full flex items-center"
+              class="cursor-pointer w-full flex items-center overflow-auto"
             >
-              <div v-if="statusFilter.length === 0">Filter by status(es)</div>
+              <div class="">
+                <div v-if="statusFilter.length === 0">
+                  <p class="pl-8 pt-3.5">Filter by status(es)</p>
+                </div>
 
-              <div
-                v-else
-                class="flex gap-[10px] border border-blue-500 w-[150px] overflow-auto"
-                tabindex="0"
-                role=""
-              >
                 <div
-                  v-for="status in statusFilter"
-                  class="flex flex-row gap-[5px] w-auto relative items-center justify-center p-0.5 me-2 text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+                  v-else
+                  class="flex gap-[8px] w-[150px] pb-4"
+                  tabindex="0"
+                  role=""
                 >
-                  <div class="text-gray-600 flex">
-                    {{ status }}
+                  <div
+                    v-for="status in statusFilter"
+                    class="mt-2.5 flex flex-row gap-[5px] w-auto items-center justify-center p-0.5 me-2 text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+                  >
                     <div
-                      @click="filterDeleteSelection(status)"
-                      class="z-[10] text-black"
+                      class="relative transition-all text-sm duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 flex"
                     >
-                      <p class="pr-1">&nbsp;&nbsp;X</p>
+                      {{ status }}
+                      <div
+                        @click="filterDeleteSelection(status)"
+                        class="z-[10]"
+                      >
+                        <p class="pr-1 hover:text-red-500">&nbsp;&nbsp;X</p>
+                      </div>
                     </div>
                   </div>
                 </div>
