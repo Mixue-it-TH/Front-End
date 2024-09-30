7e750cc05c576345cbb0cb8a6530e3942d098df9
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-09-30 19:14:37 +0700

    update for alert permission

--- src/compasable/useCheckPrivate.js
+++ src/compasable/useCheckPrivate.js
@@ -1,4 +1,4 @@
-import {ref, watch} from "vue";
+import {ref, watchEffect} from "vue";
 import {useAlert} from "../store/alert";
 
 export function useCheckPrivate() {
@@ -6,11 +6,15 @@ export function useCheckPrivate() {
   const isPrivate = ref(false);
 
   const checkLocalStorage = () => {
-    isPrivate.value = localStorage.getItem("isPrivate") !== null; // เปลี่ยนเป็น Boolean
+    isPrivate.value = localStorage.getItem("isPrivate") !== null;
   };
 
-  watch(isPrivate, (newValue) => {
-    if (newValue) {
+  checkLocalStorage();
+
+  watchEffect(() => {
+    isPrivate.value = localStorage.getItem("isPrivate") !== null;
+
+    if (isPrivate.value) {
       alertManagement.statusHandler(
         "error",
         "Access denied, you do not have permission to view this page."
