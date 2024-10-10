bd074acc800ad144ef4b58e9b974639b2a7caf1a
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-10-10 15:19:23 +0700

    implementing UI for board

--- src/components/Board/Board.vue
+++ src/components/Board/Board.vue
@@ -20,7 +20,24 @@ const color = randomColor();
   <div
     class="bg bg-white flex flex-col w-auto h-[225px] rounded-[10px] duration-300 transform hover:scale-[101%] hover:shadow-xl"
   >
-    <div class="flex flex-col px-[20px] pb-[10px] h-[50%] bg rounded-t-[10px]" :class="`${color.bg_color}`">
+    <div
+      class="flex flex-col px-[20px] pb-[10px] h-[50%] bg rounded-t-[10px] cursor-pointer hover:show-svg"
+      :class="`${color.bg_color}`"
+    >
+      <div v-if="board?.owner?.name" class="svg-container m-auto mt-[40px] duration-200">
+        <svg
+          class="svg-icon"
+          style="width: 30px; height: 30px; vertical-align: middle; fill: white; overflow: hidden"
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M160 128A96 96 0 0 0 64 224v576A96 96 0 0 0 160 896h262.72a374.464 374.464 0 0 1-25.216-64H160a31.872 31.872 0 0 1-32-32v-59.264l227.52-256.512L430.016 562.56c10.752-19.008 23.232-36.992 37.248-53.504L353.92 389.76 128 644.224V224c0-17.728 14.272-32 32-32h704c17.728 0 32 14.272 32 32v198.72c22.72 11.776 44.48 25.536 64 41.792V224A96 96 0 0 0 864 128zM704 256c-35.2 0-64 28.8-64 64s28.8 64 64 64 64-28.8 64-64-28.8-64-64-64z m32 192C577.28 448 448 577.28 448 736S577.28 1024 736 1024s288-129.28 288-288S894.72 448 736 448z m0 64c124.032 0 224 100.032 224 224 0 124.032-100.032 224-224 224A223.616 223.616 0 0 1 512 736C512 611.968 612.032 512 736 512zM704 576v128H576v64h128v128h64v-128h128v-64h-128V576z"
+            fill=""
+          />
+        </svg>
+      </div>
       <div
         class="itbkk-board-visibility itbkk-access-right mt-auto bg bg-red-200 rounded-[30px] overflow-hidden px-[8px] py-[3px] text-white w-fit"
         :class="board.visibility === 'PUBLIC' ? `${color.PUBLIC}` : `${color.PRIVATE}`"
@@ -48,4 +65,15 @@ const color = randomColor();
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.svg-container {
+  opacity: 0; /* Hide the SVG by default */
+  transform: translateY(10px); /* Slightly move down */
+  transition: opacity 0.3s ease, transform 0.3s ease; /* Transition effect */
+}
+
+.hover\:show-svg:hover .svg-container {
+  opacity: 1; /* Show the SVG on hover */
+  transform: translateY(0); /* Reset position */
+}
+</style>
