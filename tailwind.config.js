afc5eb281cd04931b4aa119846dd812edfb27aff
Xsmity <111405516+Xsmitylnwza@users.noreply.github.com>
2024-10-30 20:17:45 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	theme: {
-		screens: {
-			'mobile': { 'max': '320px' },
-			'mobile-M': { 'max': '376px' },
-			'mobile-L': { 'max': '426px' },
-			'tablet': { 'max': '768px' },
-			'laptop': { 'max': '1024px' },
-			'laptop-L': { 'max': '1440px' },
-		},
-		extend: {
-			fontFamily: {
-				inter: "'Inter', sans-serif",
-				istok: "'Istok Web', sans-serif",
-				permenent: "'Permanent Marker', cursive",
-				nonto: "'Noto Sans JP', 'Noto Sans Thai'"
-			}
-		},
-	},
-
-	plugins: [require('daisyui')],
-}
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      'mobile': { 'max': '320px' },
+      'mobile-M': { 'max': '376px' },
+      'mobile-L': { 'max': '426px' },
+      'tablet': { 'max': '768px' },
+      'laptop': { 'max': '1024px' },
+      'laptop-L': { 'max': '1440px' },
+    },
+    extend: {
+      fontFamily: {
+        inter: "'Inter', sans-serif",
+        istok: "'Istok Web', sans-serif",
+        permenent: "'Permanent Marker', cursive",
+        nonto: "'Noto Sans JP', 'Noto Sans Thai'",
+      },
+    },
+  },
+  plugins: [daisyui],
+};
